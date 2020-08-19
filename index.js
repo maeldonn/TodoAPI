e88dcaa859fb7bc84b618743873904e8b4f5fdd5
Maël Donnart <57325246+maeldonn@users.noreply.github.com>
2020-08-19 22:10:49 +0200

    API routes are functional

--- index.js
+++ index.js
@@ -1,7 +1,105 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('Salut tout le monde !');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Todo = require('./models/todo');
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+mongoose
+  .connect('mongodb://localhost:27017/todo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connexion to MongoDB successful !'))
+  .catch(() => console.log('Connexion to MongoDB failed !'));
+
+// Page d'acceuil de l'API
+// TODO : Documentation
+app.get('/', (req, res) => {
+  res.send('Home');
 });
-server.listen(8080);
+
+// Récupérer la liste de toutes les tâches
+// GET /todo
+app.get('/todo/', async (req, res) => {
+  const todo = await Todo.find();
+  res.json(todo);
+});
+
+// Récupérer la liste de toutes les tâches terminées
+// GET /todo/completed
+app.get('/todo/completed/', async (req, res) => {
+  const todo = await Todo.find({ completed: true });
+  res.json(todo);
+});
+
+// Récupérer la liste de toutes les tâches non terminées
+// GET /todo/tocomplete
+app.get('/todo/tocomplete/', async (req, res) => {
+  const todo = await Todo.find({ completed: false });
+  res.json(todo);
+});
+
+// Récupérer une tâche avec son identifiant
+// GET /todo/:id
+app.get('/todo/:id', async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const todo = await Todo.findById(id);
+  res.json(todo);
+});
+
+// Ajouter une tâche
+// POST /todo
+app.post('/todo/', async (req, res) => {
+  let { title, completed } = req.body;
+  if (title === undefined) {
+    title = '';
+  } else if (completed === undefined) {
+    completed = false;
+  }
+  const newTask = new Todo({
+    title,
+    completed,
+  });
+  await newTask.save();
+  res.json(newTask);
+});
+
+// Modifier une tâche
+// PATCH /todo
+app.patch('/todo/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, completed } = req.body;
+  const todo = await Todo.findById(id);
+  if (title) {
+    todo.title = title;
+  }
+  if (completed) {
+    todo.completed = completed;
+  }
+  await todo.save();
+  res.json(todo);
+});
+
+// Supprimer une tâche
+// DELETE /todo/:id
+app.delete('/todo/:id', async (req, res) => {
+  const suppr = await Todo.findByIdAndDelete(req.params.id);
+  res.json(suppr);
+});
+
+// Supprimer toutes les tâches
+// DELETE /todo
+app.delete('/todo/', async (req, res) => {
+  const suppr = await Todo.deleteMany({});
+  res.json(suppr);
+});
+
+app.listen(8080);
